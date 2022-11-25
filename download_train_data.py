c3418f26abbbb5042b6ff84c3c994613e46743f2
gluver <credog@foxmail.com>
2022-11-25 21:55:21 +0300

    initial commit

--- download_train_data.py
+++ download_train_data.py
@@ -6,9 +6,13 @@
 # Created Time: Mon Oct 17 11:58:02 2022
 
 import os
+import threading
+import logging
+from queue import SimpleQueue
+
 
 if __name__ == "__main__":
-    for i in range(10):
+    for i in range(100):
         name = '0' * (5 - len(str(i))) + str(i)
         cmd = "wget " + " https://searchscience.baidu.com/baidu_ultr/train_click/part-" + name + ".gz"
         print(cmd)
